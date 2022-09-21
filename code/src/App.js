ac2a585e67a4932c0b96aafcf1a3256976d0750b
Ulrika Öhman <ulrikaohman@MacBook-Pro-som-tillhor-Ulrika.local>
2022-09-21 21:08:30 +0300

    Added Header component

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import data from "./data.json";
 import Album from "./components/Album";
+import Header from "./components/Header";
 
 console.log(data);
 
 export const App = () => {
   return (
     <div>
+
+      <Header/>
       
       {data.albums.items.map(item => {
         return <Album 
---
+++ code/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+
+    return (
+        <div>
+
+            <h1>
+            New albums & singles
+            </h1>
+        </div>
+    )
+}
+
+
+export default Header;
